252600a92275fa150b12ed61aea1e458debe0c7b
Henrik Wolf <henrik-wolf@freenet.de>
2023-03-14 21:48:01 +0100

    fixed test with current version of tile providers

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,25 @@
-using Dates
 using GADM
 using Leaflet
 using Test
 using WebIO
 
 providers = (
-    Leaflet.OpenStreetMap(),
-    Leaflet.OpenStreetMap(:France),
-    Leaflet.OpenStreetMap(:DE),
-    Leaflet.OpenTopoMap(),
-    Leaflet.CartoDB(:dark_nolabels),
-    Leaflet.Esri(),
-    Leaflet.Stamen(),
-    Leaflet.Stamen(:watercolor),
-    Leaflet.CartoDB(:dark_all),
-    Leaflet.Google(:hybrid),
-    Leaflet.MapBox(; tileset_id="username.id", access_token="sometoken"),
-    Leaflet.Jawg(; access_token="sometoken"),
-    Leaflet.Thunderforest(; apikey="someapikey"),
-    Leaflet.OpenWeatherMap(:clouds; apikey="someapikey"),
-    Leaflet.OpenWeatherMap(:clouds; apikey="someapikey"),
-    Leaflet.NASAGIBS(:VIIRS_CityLights_2012),
-    Leaflet.NASAGIBS(:AMSRE_Brightness_Temp_89H_Day; date=Date(2010, 05, 07)),
-    Leaflet.Provider("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+    Providers.OpenStreetMap(),
+    Providers.OpenStreetMap(:France),
+    Providers.OpenStreetMap(:DE),
+    Providers.OpenTopoMap(),
+    Providers.CartoDB(:DarkMatterNoLabels),
+    Providers.Esri(),
+    Providers.Stamen(),
+    Providers.Stamen(:Watercolor),
+    Providers.CartoDB(:DarkMatter),
+    Providers.Google(:hybrid),
+    Providers.MapBox(; accesstoken="sometoken"),
+    Providers.Jawg(; accesstoken="sometoken"),
+    Providers.Thunderforest(; apikey="someapikey"),
+    Providers.OpenWeatherMap(:Clouds),
+    Providers.NASAGIBS(:ViirsEarthAtNight2012),
+    Providers.Provider("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
 )
 
 # We are not actually testing that the web interface shows the map,
@@ -30,13 +27,13 @@ providers = (
 # actually works in a browser is a much bigger task.
 for provider in providers
     # Make a country outline Layer
-    layers = Leaflet.Layer(GADM.get("MUS").geom[1]; 
-        color="#ff0201", 
+    layers = Leaflet.Layer(GADM.get("MUS").geom[1];
+        color="#ff0201",
         opacity=0.6,
-        fill_opacity=0.2,
+        fill_opacity=0.2
     )
     # Combine into a Map
-    m = Leaflet.Map(; layers, provider, zoom=3, height=1000);
+    m = Leaflet.Map(; layers, provider, zoom=3, height=1000)
     # Render as html/javascript
     interface = WebIO.render(m)
 end
