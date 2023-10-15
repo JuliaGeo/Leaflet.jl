189c152df25c04907382898dc34d39bc132a7402
Martijn Visser <mgvisser@gmail.com>
2023-10-15 21:47:30 +0200

    get docs building again
    Docs were not building on Documenter 1 due to the repo link, but this doesn't seem needed, since the GitHub link in the rendered docs points to the right location.
    
    This removes the accidentally committed docs/build, and updates .gitignore to ensure that won't happen again.
    
    Updated the "Manual test" code and made sure it is working.

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,6 @@ using Documenter
 
 makedocs(;
     modules=[Leaflet],
-    repo="https://github.com/JuliaGeo/Leaflet.jl/blob/{commit}{path}#{line}",
     sitename="Leaflet.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
